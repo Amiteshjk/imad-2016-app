ea6fbb18aa068d1cbf41f132fbc45a0bf5c299d7
Amiteshjk <jkamitesh99@gmail.com>
2016-09-21 19:06:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var articles={
  title:'article-one | Amitesh',
     heading: 'article one',
     content:` 
+     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <p>
     <img src="http://i.dailymail.co.uk/i/pix/2013/01/01/article-2255743-16B7C024000005DC-691_634x424.jpg"/>
     </p>
@@ -23,6 +24,7 @@ var articles={
 title:'article two | Amitesh',
 heading:'article two',
 content:`
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
 <p>
         <img src="http://i.ndtvimg.com/i/2016-02/joey_640x480_71454592517.jpg"/>
         </p>
@@ -39,6 +41,7 @@ content:`
 title: 'article-three | Amitesh',
 heading:'article three',
 content:`
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
      <p>
      <img src="https://lh3.googleusercontent.com/nMWJxniNCSytb9ppkz1bDKjPHTLhRZycqoic7a0roHlNs6dPr7jiVZKw2xDQMyEnYW2oRPZEN67S2yudTGTv40nWWslegeILYn-vEkftdX1J4Sz6ajp6ZVSQkoft1z0HJu1jp6W4cvYcPk6oGqhRKrPzeW4FlLmGwzWwjiZBiaUxKgGbNfq_Rrlbd0mzQ9USM3mAADyv50uCWm-VWLGGqJcyAqcgwNKstOqZ7Mwq4Y5llQ_NOsNes_-PeSkZestDxQYAOcwqm2OqIHOP4R81SzsJJiOoFm4ZFbqLg7RfXNyMD7dmerQ6XkpRfdYMvs7tpYDzKeHJtvSNMLUzgI5fS8mGaAm5JWntk_cRBBuglkQSN0yIfd6bKmNHjqRXA--i0bQiJXrMOmooFsnbNnfBf0q_A4l6C9aWA6ssa9FgcDSXeQgY84IyWtsFyBvSqLQaPnaDy6vZI22fWz3VEwMR7RdBlGQjXtUItbP691r1dIrngdty799kw8ZAm_nY5ExdOpaVLlFEzZq5EObWR7WgGFFhHEhAF4ptPBnlEDXlXEy3IidNb-o4dhPxVD-NmrKC-WvWVUGura69i-aqICfjmuBvXGLM9_E2hId2DwdUR6EQoW5h=w225-h338-no"  />
      </p>
