1dcb608e29bf665f9dcd4c20c819ae16c3be716a
Amiteshjk <jkamitesh99@gmail.com>
2016-09-26 21:23:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,3 @@
 
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New value'; 
 
-var img = document.getElementById('friends');
-img.onclick = function (){
-    img.style.marginLeft = '100px';
-    
-};
