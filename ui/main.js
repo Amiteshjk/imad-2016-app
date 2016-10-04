fb603104ef358bf3f61d0eaee77dc86fc0712dd6
Amiteshjk <jkamitesh99@gmail.com>
2016-10-04 22:56:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@ console.log('Loaded!');
 var img = document.getElementById('friends');
 var marginLeft=0;
     function moveRight(){
-        marginLeft=marginLeft+5;
+        marginLeft=marginLeft+2;
         img.style.marginLeft=marginLeft+
         'px';
     }
     img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+    var interval=setInterval(moveRight,10);
     
 };
