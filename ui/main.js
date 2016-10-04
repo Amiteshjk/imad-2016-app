a9ee85dc9071d6acb6ab8beb9c11c1f69a1838a3
Amiteshjk <jkamitesh99@gmail.com>
2016-10-04 23:40:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,9 @@
-console.log('Loaded!');
-
-var img = document.getElementById('friends');
-var marginLeft=0;
-    function moveRight(){
-        marginLeft=marginLeft+2;
-        img.style.marginLeft=marginLeft+
-        'px';
-    }
-    img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+var button=document.getElementById('counter');
+button.onclick=function(){
     
+    
+    
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
