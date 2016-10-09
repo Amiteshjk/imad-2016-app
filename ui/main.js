499c1bdbde4734dec693707519029da9f5838c56
Amiteshjk <jkamitesh99@gmail.com>
2016-10-09 14:16:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             
             if(request.status===200) {
-                var name=request.responseText;
+                var names=request.responseText;
                 names=JSON.parse(names);
                 var list='';
                 for(var i=0;i<names.length;i++){
